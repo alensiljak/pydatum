910d4d8c118b2a6198c0f0b421ab768d442fd65c
Alen Siljak <alen.siljak@gmx.com>
2018-03-13 11:07:10 +0100

    testing the initial value

--- tests/test_datum.py
+++ tests/test_datum.py
@@ -6,4 +6,11 @@ def test_instantiation():
     """ Test creation of the object """
     actual = Datum()
     assert actual is not None
-    
+
+def test_init_value():
+    """ The initial value should be now """
+    from datetime import datetime
+
+    datum = Datum()
+    now = datetime.now()
+    assert now == datum.value
