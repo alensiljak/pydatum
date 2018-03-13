5391baba462e1ff6e7d043104ae071bd90ba2499
Alen Siljak <alen.siljak@gmx.com>
2018-03-13 11:15:11 +0100

    a few tests

--- tests/test_datum.py
+++ tests/test_datum.py
@@ -1,6 +1,6 @@
 """ Test Datum functions """
 from pydatum import Datum
-
+from datetime import datetime
 
 def test_instantiation():
     """ Test creation of the object """
@@ -9,8 +9,29 @@ def test_instantiation():
 
 def test_init_value():
     """ The initial value should be now """
-    from datetime import datetime
-
     datum = Datum()
     now = datetime.now()
     assert now == datum.value
+
+def test_add_days():
+    datum = Datum()
+    datum.today()
+    datum.start_of_day()
+
+    # today_str = datum.get_iso_date_string()
+    today = datetime.today()
+    day = today.day + 1
+    tomorrow = today.replace(day=day, hour=0, minute=0, second=0, microsecond=0)
+    
+    actual = datum.add_days(1)
+    
+    assert actual == tomorrow
+
+def test_start_of_day():
+    datum = Datum()
+    datum.start_of_day()
+    actual = datum.value
+
+    assert actual.hour == 0
+    assert actual.minute == 0
+    assert actual.second == 0
