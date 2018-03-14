9d45cbbd56adb46def993869939eb07b8636750d
Alen Siljak <alen.siljak@gmx.com>
2018-03-14 09:13:55 +0100

    a few more tests

--- tests/test_datum.py
+++ tests/test_datum.py
@@ -1,6 +1,7 @@
 """ Test Datum functions """
 from pydatum import Datum
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 def test_instantiation():
     """ Test creation of the object """
@@ -18,10 +19,10 @@ def test_add_days():
     datum.today()
     datum.start_of_day()
 
-    # TODO this will fail at the end of the month!
     today = datetime.today()
-    day = today.day + 1
-    tomorrow = today.replace(day=day, hour=0, minute=0, second=0, microsecond=0)
+    #day = today.day + 1
+    tomorrow = today + relativedelta(days=1)
+    tomorrow = tomorrow.replace(hour=0, minute=0, second=0, microsecond=0)
     
     actual = datum.add_days(1)
     
@@ -35,3 +36,18 @@ def test_start_of_day():
     assert actual.hour == 0
     assert actual.minute == 0
     assert actual.second == 0
+
+def test_clone():
+    one = Datum()
+    two = one.clone()
+
+    assert one.value == two.value
+
+def test_from_iso_date_string():
+    str_value = "2017-08-23"
+    datum = Datum()
+    datum.from_iso_date_string(str_value)
+
+    assert datum.value.day == 23
+    assert datum.value.month == 8
+    assert datum.value.year == 2017
