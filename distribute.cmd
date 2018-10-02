8c483057889c346142b976150bcf3face3fe4e89
Alen Siljak <alen.siljak@gmx.com>
2018-10-02 12:37:16 +0200

    v1.0.0

--- distribute.cmd
+++ distribute.cmd
@@ -7,12 +7,9 @@
 
 :: Clean-up the destination
 del dist\*
-pause
 
 :: Create the binary package.
 setup.py sdist bdist_wheel
-
-pause
 
 :: Deploy to test server.
 twine upload -u cicko --repository-url https://test.pypi.org/legacy/ dist/*
@@ -21,3 +18,5 @@ pause
 
 :: Deploy to prod server.
 twine upload -u cicko --repository-url https://upload.pypi.org/legacy/ dist/*
+
+pause
